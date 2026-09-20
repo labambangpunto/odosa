7bec509fb05ea6bfc1670af31dd1f266b3ed6d7c
labambangpunto <323229534+labambangpunto@users.noreply.github.com>
2026-09-20 20:37:50 +0700

    kibulin lefthook wkwkw

---
+++ test/dummy_test.dart
@@ -0,0 +1,8 @@
+import 'package:flutter_test/flutter_test.dart';
+
+void main() {
+  test('Tes formalitas untuk Lefthook', () {
+    // Memastikan 1 + 1 = 2 (pasti selalu benar/lolos)
+    expect(1 + 1, 2);
+  });
+}
